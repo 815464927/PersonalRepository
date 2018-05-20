1413a871cdc285ba5daddc7f8f0d5cc3f1b999b6
Song <1034966089@qq.com>
2018-05-20 15:48:24 +0800

    path.xxxTo 画线

--- app/src/main/java/com/song/myapplication/PathView.java
+++ app/src/main/java/com/song/myapplication/PathView.java
@@ -41,6 +41,49 @@ public class PathView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //canvas.drawPath(path,paint);
+
+        //addCircle(canvas);
+
+        LineTo(canvas);
+    }
+
+    /**
+     * xxxTo() ——画线（直线或曲线）
+     * 画线
+     * @param canvas 画布
+     */
+    private void LineTo(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        Path path = new Path();
+        path.lineTo(100,100);//由当前位置（0,0）向（100,100）画一条直线
+
+//        path.rLineTo(200,0);//相对于（100,100），向有200个像素的位置画一条直线
+//        path.moveTo(400,100);
+//        path.lineTo(400,200);
+
+        //path.arcTo(100,100,300,300,-90,90,true);
+        //path.addArc(100,100,300,300,-90,90);
+
+        path.moveTo(300,300);
+        path.lineTo(600,300);
+        path.lineTo(450,500);
+        path.close();
+
+        canvas.drawPath(path,paint);
+
+    }
+
+    /**
+     * addXxx() ——添加子图形
+     * 画一个圆
+     * @param canvas 画布
+     */
+    private void addCircle(Canvas canvas) {
+        Paint paint = new Paint();
+        Path path = new Path();
+        path.addCircle(100,100,50, Path.Direction.CCW);
         canvas.drawPath(path,paint);
     }
 }
